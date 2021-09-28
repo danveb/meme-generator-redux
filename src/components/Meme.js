816bf1d1a011c6a34824d970dcb337d8642772fe
Daniel Bae <danibae91@icloud.com>
2021-09-28 10:13:15 -0700

    add classNames to different elements for styling

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,13 +1,14 @@
 import React from 'react' 
+import '../static/styles/Meme.css' 
 
 const Meme = ({ id, url, topText, bottomText }) => {
     return (
         <div>
-            <div className="container">
-                <span>{topText}</span>
+            <div className="Meme-container">
+                <span className="Meme-topText">{topText}</span>
                 <img src={url} alt="meme" />
-                <span>{bottomText}</span>
-                <button>X</button>
+                <span className="Meme-bottomText">{bottomText}</span>
+                <button className="Meme-deleteBtn">X</button>
             </div>
 
         </div>
